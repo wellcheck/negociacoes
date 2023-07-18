62714043890663d07a97a7c9d0ef00caec0ef591
wellcheck <wellington.check@gmail.com>
2023-07-18 19:37:41 -0300

    Implementando views

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import { Negociacao } from "./models/negociacao.js";
 import { NegociacaoController } from "./controllers/negociacao_controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 
@@ -7,3 +8,6 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', (event:Event) => {
     event.preventDefault();
     controller.adiciona();});
+
+
+
